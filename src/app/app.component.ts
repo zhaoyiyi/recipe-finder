d9c530bb928882f4f0e1097e14fd9313fb170ab0
Yi Zhao <yi@yizhao.me>
2017-03-05 16:23:20 -0500

    add link to vue version in header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
         <span class="title">Recipe Finder</span>
         <a md-button routerLink="/search">Search</a>
         <a md-button routerLink="/shopping-list">Shopping List</a>
+        <span style="flex-grow: 1"></span>
+        <a md-button href="http://recipe-finder-vue.yizhao.me">Vue.js Version</a>
       </md-toolbar>
     </nav>
     <router-outlet></router-outlet>
